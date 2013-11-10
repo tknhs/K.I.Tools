07e5b9b27d7d5064d693d8c78b2a3fb42562bd24
tknhs <kdsnlv0109@gmail.com>
2013-11-10 19:40:58 +0900

    カレンダー ・ポータルの自動ログインが有効時にカレンダー同期のページアクションを表示 ・カレンダー同期完成（変則日程と大学の行事） UIの修正 ・カレンダー同期のページアクション

--- kitools/js/background.js
+++ kitools/js/background.js
@@ -11,14 +11,19 @@ _gaq.push(['_trackPageview']);
 // インストール時
 chrome.storage.local.get(function(items){
   if (items.general === undefined){
-    _gaq.push(['_trackEvent', 'Installed']);
     chrome.storage.local.set({'general': new Array(false, true, false)});
   }
 });
 
 // ページアクションを表示
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.indexOf('http://portal10.mars.kanazawa-it.ac.jp/portal/student') != -1) {
-    chrome.pageAction.show(tabId);
+  if (tab.url.indexOf('http://portal10.mars.kanazawa-it.ac.jp/portal/student') == 0 ||
+      tab.url.indexOf('https://ras.kanazawa-it.ac.jp/portal/,DanaInfo=portal10.mars.kanazawa-it.ac.jp+student') == 0) {
+    chrome.storage.local.get(function(items){
+      // ポータルの自動ログインが有効
+      if (items.general[0]){
+        chrome.pageAction.show(tabId);
+      }
+    });
   }
 });
